80023f66cec5e2c4a3e4af08f69a2d69cd68445f
Tolibjon <tolibjohn@gmail.com>
2022-01-16 23:23:13 +0500

    Create connection with mongodb

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://toliboff:parol2021@cluster0.iqhjv.mongodb.net/Projet0?retryWrites=true&w=majority").then(()=>console.log('Connection success'));
 
 const app = express();
 
