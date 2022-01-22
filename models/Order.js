e2f4e9d9f5755e1db2ddd1f6fe2a1f5260587ee5
Tolibjon <tolibjohn@gmail.com>
2022-01-22 23:45:09 +0500

    Add addition fields for Order model

--- models/Order.js
+++ models/Order.js
@@ -10,6 +10,9 @@ const OrderSchema = new mongoose.Schema(
         quantity: {type: Number, default: 1}
       }
     ],
+    amount:{type: Number, required: true},
+    address: {type: Object, required: true},
+    status: {type: String, default: 'pending'},
   },
   {timestamps: true}
 );
