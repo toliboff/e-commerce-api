69a2ce34c65755592fa38cf5ebbd08a5a3255ed2
Tolibjon <tolibjohn@gmail.com>
2022-01-25 19:59:48 +0500

    Add login functionality

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv  = require("dotenv");
 const app = express();
-const userRoute = require('./routes/user')
-const authRoute = require('./routes/auth')
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
@@ -13,6 +13,10 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
+app.get('/api/test', (req, res)=>{
+  res.send("TEst test test");
+})
+
 
 app.listen(process.env.PORT || 5000, ()=>{
   console.log("Backend server is running");
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const router = require("express").Router()
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 router.post("/register", async (req, res) =>{
-  const newUser = newUser({
+  const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.PASSWORD_SECRET).toString(),
   });
 
   try {
@@ -15,5 +16,21 @@ router.post("/register", async (req, res) =>{
     res.status(500).json(err);
   }
   
+})
+ 
+
+router.post("/login", async (req, res) => {
+  try {
+    const user  = await User.findOne({username: req.body.username})
+    !user && res.status(500).json("Wrong credentials");
+    const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASSWORD_SECRET);
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    password !== req.body.password && res.status(401).json("Wrong credentials");
+
+    res.status(400).json(user)
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
 })
 module.exports = router;
