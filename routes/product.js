25b2cddbabc5600316f5b5b2c29c06d9d2264736
Tolibjon <tolibjohn@gmail.com>
2022-01-27 18:39:09 +0500

    Create action to delete the product

--- routes/product.js
+++ routes/product.js
@@ -30,15 +30,15 @@ router.put("/:id", verifyTokenAndAdmin, async (req, res)=>{
   }
 })
 
-// // DELETE
-// router.delete("/:id", verifyTokenAndAuthorisation, async (req, res)=>{
-//   try {
-//    await User.findByIdAndDelete(req.params.id);
-//    res.send(200).json("User has been deleted");
-//   } catch (error) {
-//     res.status(500).json(error)
-//   }
-// })
+// DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res)=>{
+  try {
+   await Product.findByIdAndDelete(req.params.id);
+   res.send(200).json("Product has been deleted");
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 
 // // GET USER
 // router.get("/find/:id", verifyTokenAndAdmin, async (req, res)=>{
