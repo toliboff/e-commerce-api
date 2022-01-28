e63eea249f01c1c2aae25f3a97906213d9476a3a
Tolibjon <tolibjohn@gmail.com>
2022-01-28 20:44:35 +0500

    Create  actions for orders

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ app.use(express.json())
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
 app.get('/api/test', (req, res)=>{
   res.send("TEst test test");
 })
