2bd1b29bd4d91313ce3ffc5508b825471f3b8f60
Tolibjon <tolibjohn@gmail.com>
2022-01-28 15:36:45 +0500

    Create action to get products

--- routes/product.js
+++ routes/product.js
@@ -50,16 +50,28 @@ router.get("/find/:id", async (req, res)=>{
   }
 });
 
-// // GET ALL USERS
-// router.get("/", verifyTokenAndAdmin, async (req, res)=>{
-//   try {
-//     const query = req.query;
-//    const users = query ? await User.find().sort({_id: -1}).limit(10): await User.find();
-//    res.status(200).json(users);
-//   } catch (error) {
-//     res.status(500).json(error)
-//   }
-// });
+// GET ALL PRODUCTS
+router.get("/", async (req, res)=>{
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if(qNew){
+      products = await Product.find().sort({createdAt: -1}).limit(3);
+    } else if(qCategory){
+      products = await Product.find({categories: {
+        $in: [qCategory],
+      }})
+    } else {
+      products = await Product.find();
+    }
+    
+   res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
 
 // // GET USER STATS
 // router.get("/stats", verifyTokenAndAdmin, async (req, res)=>{
