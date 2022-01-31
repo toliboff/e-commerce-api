84e69a2ba72f0ba4c2131dd23bb128ad47f4e11b
Tolibjon <tolibjohn@gmail.com>
2022-01-31 16:40:05 +0500

    Add stripe payment

---
+++ routes/stripe.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+router.post("/payment", (req, res)=>{
+  stripe.charges.create({
+    source: req.body.tokenId,
+    amount: req.body.amount,
+    currency: "usd",
+  }, (stripeErr, stripeRes) => {
+    if(stripeErr){
+      res.json.status(500).json(stripeErr);
+    } else {
+      res.status(200).json(stripeRes);
+    }
+  })
+})
+module.exports = router;
