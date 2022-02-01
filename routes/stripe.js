c4859f4278d74b2ffb01aff4cf27c3dbe26915e5
Tolibjon <tolibjohn@gmail.com>
2022-02-01 20:18:09 +0500

    Add stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res)=>{
   stripe.charges.create({
@@ -14,4 +14,4 @@ router.post("/payment", (req, res)=>{
     }
   })
 })
-module.exports = router;
+module.exports = router; 
