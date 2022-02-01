338727bc7438e0763f287a23675f2231952f8e9e
Tolibjon <tolibjohn@gmail.com>
2022-02-01 20:18:24 +0500

    Add checkout route

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
-
-dotenv.config();
+const stripeRoute = require('./routes/stripe');
+const cors = require("cors");
+dotenv.config(); 
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log('DB connection successfull!'))
 .catch(e=>console.log(e))
+
+app.use(cors());
 
 app.use(express.json())
 app.use('/api/auth', authRoute)
@@ -20,9 +23,8 @@ app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/carts', cartRoute)
 app.use('/api/orders', orderRoute)
-app.get('/api/test', (req, res)=>{
-  res.send("TEst test test");
-})
+app.use('/api/checkout', stripeRoute)
+
 
 
 app.listen(process.env.PORT || 5000, ()=>{
